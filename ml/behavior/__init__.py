9549e5e23294f451c687b3544aa6b8e5550b9413
Ashutosh-0511 <ashutoshpandey0511@gmail.com>
2026-09-14 02:30:39 +0530

    feat: expose X3D behavior backend

--- ml/behavior/__init__.py
+++ ml/behavior/__init__.py
@@ -1,4 +1,9 @@
 from .behavior_mapper import BehaviorMapper
 from .videomae import VideoMAEBehaviorClassifier
+from .x3d import X3DBehaviorClassifier
 
-__all__ = ["BehaviorMapper", "VideoMAEBehaviorClassifier"]
+__all__ = [
+    "BehaviorMapper",
+    "VideoMAEBehaviorClassifier",
+    "X3DBehaviorClassifier",
+]
